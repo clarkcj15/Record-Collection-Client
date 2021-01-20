f6587d8f2c7256061f348f0c54371055c94b2e5e
CJ Clark <cj.clark915@gmail.com>
2021-01-20 15:08:49 -0600

    single album show page and edit

--- record-collection-frontend/src/Components/Albums/SingleAlbum.js
+++ record-collection-frontend/src/Components/Albums/SingleAlbum.js
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from 'react'
 
 
 const SingleAlbum = () => {
@@ -7,5 +8,5 @@ const SingleAlbum = () => {
         </div>
     )
 }
-
+// Delete & Edit
 export default SingleAlbum;
